b477f8de282692e03e1bb01a047285bff604a57e
erixhens <erixhens.muka1@protonmail.com>
2024-03-17 21:43:30 +0100

    :tada: feat: create remix-vite-tailwind-shadcn project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { ModeToggle } from '~/components/ui/mode-toggle';
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +12,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <ModeToggle />
       <ul>
         <li>
           <a
